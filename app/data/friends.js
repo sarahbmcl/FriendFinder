71803417dbb97980ec1fb0caa4c37d2b6def884a
Sarah McLaughlin <sarahbmcl@gmail.com>
2019-12-03 20:01:51 -0600

    add friend data

---
+++ app/data/friends.js
@@ -0,0 +1,171 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+
+var friends = [
+    {
+      name: "Rachel Green",
+      photo: "https://media.licdn.com/mpr/mpr/http://images.hellogiggles.com/uploads/2016/01/07/Friends-Rachel-Green-Kitchen.jpg/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Joey Tribbiani",
+      photo: "https://pbs.https://upload.wikimedia.org/wikipedia/en/d/da/Matt_LeBlanc_as_Joey_Tribbiani.jpg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "3",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Phoebe Buffay",
+      photo: "https://avatars2.https://upload.wikimedia.org/wikipedia/en/f/f6/Friendsphoebe.jpg.com/u/8504998?v=3&s=460",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Monica Geller",
+      photo: "https://pbs.twimg.com/https://upload.wikimedia.org/wikipedia/en/d/d0/Courteney_Cox_as_Monica_Geller.jpg/639214960049000449/lNCRC-ub.jpg",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "Chandler Bing",
+      photo: "https://media.licdn.com/mpr/mpr/https://upload.wikimedia.org/wikipedia/en/6/66/Matthew_Perry_as_Chandler_Bing.png/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Ross Geller",
+      photo: "https://media.licdn.https://upload.wikimedia.org/wikipedia/en/6/6f/David_Schwimmer_as_Ross_Geller.jpg/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+      scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    },
+    {
+        name: "Will Smith",
+        photo: "https://media.licdn.com/mpr/mpr/https://upload.wikimedia.org/wikipedia/en/2/22/Will_Smith_Fresh_Prince.jpeg/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+        scores: [
+          "5",
+          "5",
+          "5",
+          "5",
+          "5",
+          "2",
+          "5",
+          "3",
+          "1",
+          "3"
+        ]
+      },
+      {
+        name: "Carlton Banks",
+        photo: "https://media.licdn.com/mpr/https://vignette.wikia.nocookie.net/freshprince/images/a/ac/1carlton.jpg/revision/latest?cb=20180626133412/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+        scores: [
+          "3",
+          "3",
+          "4",
+          "5",
+          "5",
+          "2",
+          "1",
+          "1",
+          "1",
+          "1"
+        ]
+      },
+      {
+        name: "Hilary Banks",
+        photo: "https://media.https://vignette.wikia.nocookie.net/freshprince/images/3/3d/1hil.jpeg/revision/latest?cb=20180706022412.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+        scores: [
+          "4",
+          "4",
+          "4",
+          "2",
+          "5",
+          "2",
+          "5",
+          "1",
+          "1",
+          "3"
+        ]
+      },
+      {
+        name: "Ashley Banks",
+        photo: "https://media.licdn.https://vignette.wikia.nocookie.net/freshprince/images/e/e7/1ashley.jpg/revision/latest?cb=20180626195152/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+        scores: [
+          "3",
+          "3",
+          "3",
+          "3",
+          "4",
+          "3",
+          "4",
+          "3",
+          "2",
+          "4"
+        ]
+      }
+  ];
+  
+  // Here we export the array. This makes it accessible to other files using require.
+  module.exports = friends;
